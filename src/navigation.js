54b292b0271d4ab20bbffbafa223f1c64bfb8d61
Andrés Ruz Nieto - MAC <40019177+aruznieto@users.noreply.github.com>
2024-12-28 12:20:36 +0100

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -10,6 +10,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(~/assets/images/ea5joq.png)]"></span>
-    Edited by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> EA5JOQ</a> · All rights reserved.
+    Edited by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> EA5JOQ</a> · All rights reserved. © ${new Date().getFullYear()}
   `,
 };
